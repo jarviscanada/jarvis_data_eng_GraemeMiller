777a935d2d680dc9e90b4db412867190c001847e
Graeme Miller <graemevincentmiller@gmail.com>
2024-05-15 15:27:56 +0000

    Finished up tweaks to the host_info and usage insertion scripts. Also moved the sql directory.

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -6,6 +6,8 @@ db_name=$3
 psql_user=$4
 psql_password=$5
 
+export PGPASSWORD=$psql_password
+
 if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
@@ -18,15 +20,14 @@ memory_free=$(echo "$vmstat_mb" | tail -1 | awk -v col="4" '{print $col}')
 cpu_idle=$(echo "$vmstat_mb" | tail -1 | awk -v col="15" '{print $col}')
 cpu_kernel=$(echo "$vmstat_mb"| tail -1 | awk -v col="14" '{print $col}')
 disk_io=$(vmstat --unit M -d | tail -1 | awk -v col="10" '{print $col}')
-disk_available=$(df -BM / | tail -1 | awk -v col="4" '{print $col}')
+disk_available=$(df -BM / | tail -1 | awk -v col="4" '{print $col}' | sed 's/[^0-9]*//g')
 timestamp=$(vmstat -t | tail -1 | awk '{print $(NF-1), $NF}')
 date=$(date -d "$timestamp" +"%Y-%m-%d %H:%M:%S")
 
-id="SELECT FROM host_info
-    WHERE id IN(
-    SELECT id
-    FROM host_info
-    WHERE hostname == '$hostname')"
+query="SELECT id FROM host_info WHERE hostname = '$hostname'"
+id=$(psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$query")
 
-insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-            VALUES('$date', '$id', '$memory_free', '$cpu_idle', $cpu_kernel, '$disk_io', '$disk_available');"
+id_q=$(echo "$id" | tail -2 | head -1)
+
+insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES ('$date', $id_q, '$memory_free', '$cpu_idle', $cpu_kernel, '$disk_io', $disk_available);"
+psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$insert_stmt"
