0e62254d8133cfd90e7878e3be5e709fe93cb014
Graeme Miller <gvmiller@my.yorku.ca>
2024-07-10 12:06:12 -0400

    Work on mongoDb NoSQL project. Completed Service, Repository, and Controller classes.

---
+++ springboot/Insurance-mongo/src/main/java/ca/jrvs/insurance_api/model/Car.java
@@ -0,0 +1,66 @@
+package ca.jrvs.insurance_api.model;
+
+import java.util.Objects;
+
+public class Car {
+    private String make;
+    private String model;
+    private int maxSpeed;
+
+    // Constructors
+    public Car() {}
+    public Car(String make, String model, int maxSpeed) {
+        this.make = make;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    // Getters and Setters
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+    // ToString, Hashcode, Equals
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return maxSpeed == car.maxSpeed && Objects.equals(make, car.make) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(make, model, maxSpeed);
+    }
+}
